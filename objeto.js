d3457215163ebdf0ad463518f87ea084c8f39450
Gemma Clavero Del Moral <casagemmayraul@yahoo.es>
2023-12-09 22:23:44 -0500

    Update objeto.js

--- objeto.js
+++ objeto.js
@@ -8,12 +8,23 @@ class animal {
     verInfo() {
         document.write(this.info + <br>);
     }
+    ladrar() {
+        if (this.especie = "perro") {
+            documento.write("Wau!" <br>);
+        } else {
+            document.write("No puedo ladrar" <br>)
+        };
 };
 
 perro = new animal("perro", 5, "marron");
 gato = new animal("gato", 3, "blanco");
+pajaro = new animal("pajaro", 1, "verde");
 
 perro.verInfo();
 gato.verInfo();
+pajaro.verInfo();
+perro.ladrar();
+gato.ladrar();
+pajaro.ladrar();
 
 
